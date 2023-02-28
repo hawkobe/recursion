49a0190882914d575e4d05718250b46887898b06
Jacob Sanders <sanders.jms@gmail.com>
2023-02-28 11:13:45 -0700

    rubocop

--- lib/merge_sort.rb
+++ lib/merge_sort.rb
@@ -1,22 +1,24 @@
+# frozen_string_literal: true
+
 def merge_sort(arr)
   if arr.length < 2
     arr
   else
-    a = merge_sort(arr.slice(0, arr.length/2))
-    b = merge_sort(arr.slice(arr.length/2, arr.length))
+    a = merge_sort(arr.slice(0, arr.length / 2))
+    b = merge_sort(arr.slice(arr.length / 2, arr.length))
     merge(a, b)
   end
 end
 
 def merge(a, b, orig = [])
-  for length in (1..a.length + b.length)
-    if a.empty?
-      orig << b.shift
-    elsif b.empty?
-      orig << a.shift
-    else
-      a[0] < b[0] ? orig << a.shift : orig << b.shift
-    end
+  (1..a.length + b.length).each do |_length|
+    orig << if a.empty?
+              b.shift
+            elsif b.empty?
+              a.shift
+            else
+              (a[0] < b[0] ? a.shift : b.shift)
+            end
   end
   orig
 end
